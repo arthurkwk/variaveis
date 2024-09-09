b981eb2445994e226c0769938f02aa246a27687a
arthurkwk <127845701+arthurkwk@users.noreply.github.com>
2024-09-09 22:16:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const primeiroNome = "Arthur";
+const sobrenome = "Ferreira Martins"
+const idade = 17;
+const altura = 1.70;
+const cidadeNatal = "Curitiba";
+
+console.log("O meu primeiro nome é:")
+console.log(primeiroNome);
+console.log("O meu sobrenome é:")
+console.log(sobrenome);
+console.log("A minha idade é:")
+console.log(idade);
+console.log("A minha altura é:")
+console.log(altura);
+console.log("A minha cidade natal é:")
+console.log(cidadeNatal);
